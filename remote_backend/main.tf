31d19e3815e49d39b4baf783655836f040d0a295
expired-token <ahmam.oussama1@gmail.com>
2026-08-17 13:25:56 +0100

    fix5 - iam fixes

--- remote_backend/main.tf
+++ remote_backend/main.tf
@@ -1,17 +1,17 @@
 resource "aws_s3_bucket" "state_bucket" {
-    bucket = "remote-state-management-oaws-867637277826"
+  bucket = "remote-state-management-oaws-867637277826"
 
-    tags = {
-      Name = "Remote State Storage Bucket"
-    }
+  tags = {
+    Name = "Remote State Storage Bucket"
+  }
 }
 
 resource "aws_s3_bucket_versioning" "state_bucket_versioning" {
-    bucket = aws_s3_bucket.state_bucket.id
+  bucket = aws_s3_bucket.state_bucket.id
 
-    versioning_configuration {
-      status = "Enabled"
-    }
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "state_bucket_encryption" {
@@ -24,28 +24,28 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "state_bucket_encr
   }
 }
 
-resource aws_s3_bucket_policy "state_bucket_policy" {
-    bucket = aws_s3_bucket.state_bucket.id
-    policy = data.aws_iam_policy_document.allow_limited_access_to_state_bucket.json
+resource "aws_s3_bucket_policy" "state_bucket_policy" {
+  bucket = aws_s3_bucket.state_bucket.id
+  policy = data.aws_iam_policy_document.allow_limited_access_to_state_bucket.json
 }
 
 data "aws_iam_policy_document" "allow_limited_access_to_state_bucket" {
   statement {
     principals {
-      type = "AWS"
-      identifiers = [ "867637277826" ]
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::867637277826:role/GitHubActionsTerraformRole"]
     }
 
-    actions = [ 
+    actions = [
       "s3:GetObject",
       "s3:PutObject",
       "s3:DeleteObject",
       "s3:ListBucket"
     ]
 
-    resources = [ 
+    resources = [
       aws_s3_bucket.state_bucket.arn,
-      "${aws_s3_bucket.state_bucket.arn}/*"
+      "${aws_s3_bucket.state_bucket.arn}/*",
     ]
   }
 }
